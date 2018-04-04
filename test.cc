742414651588d52e030fce1578217632da86f51c
Chris Lalancette <clalancette@gmail.com>
2018-04-04 11:48:24 -0400

    Fix up error.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- test.cc
+++ test.cc
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[])
 {
     if ( argc < 2 ) {
-        std::cerr << "Usage: " << argv[0] << "library-name\n";
+        std::cerr << "Usage: " << argv[0] << " <library-name>\n";
         return 1;
     }
     std::cerr << "Before dlopen()...\n";
